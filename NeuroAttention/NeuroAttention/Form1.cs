00e6aa6cb86926f7558161490935aea159bfe208
enoxie <jemappellewine@windowslive.com>
2020-01-31 04:38:24 +0300

    MayaAcademia Application Updater - 1/31/2020 4:38 AM

---
+++ NeuroAttention/NeuroAttention/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpUpdate;
+
+namespace NeuroAttention
+{
+    public partial class Form1 : Form, ISharpUpdatable
+    {
+        private SharpUpdater updater;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.label1.Text = this.ApplicationAssembly.GetName().Version.ToString();
+            updater = new SharpUpdater(this);
+        }
+
+        public string ApplicationName 
+        {
+            get { return "NeuroAttention"; }
+        }
+
+        public string ApplicationID
+        {
+            get { return "NeuroAttention"; }
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get { return Assembly.GetExecutingAssembly(); }
+        }
+
+        public Icon ApplicationIcon 
+        {
+            get { return this.Icon; }
+        }
+
+        public Uri UpdateXmlLocation
+        {
+            get { return new Uri ("http://www.enesbilgi.com/test/update.xml"); }
+        }
+
+        public Form Context
+        {
+            get { return this; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updater.DoUpdate();
+        }
+    }
+}
---
+++ NeuroAttention/SharpUpdate/ISharpUpdatable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+using System.Reflection;
+
+namespace SharpUpdate
+{
+    public interface ISharpUpdatable
+    {
+        string ApplicationName { get; }
+        string ApplicationID { get; }
+        Assembly ApplicationAssembly { get; }
+        Icon ApplicationIcon { get; }
+        Uri UpdateXmlLocation { get; }
+        Form Context { get; }
+
+        
+    }
+}
